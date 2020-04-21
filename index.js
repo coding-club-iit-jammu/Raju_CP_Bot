4ea9e240180df321a8ded49bf769b289e6cbfe0d
Sahil Bansal <shlbnsl843@gmail.com>
2020-04-21 18:16:25 +0530

    Add prefix for the commands

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const PREFIX = "!"
 
 bot.once('ready', () => {
 	console.log('Ready!');
 });
 
 bot.on('message', message => {
-	if (message.content === "!ping") {
+	if (!message.content.startsWith(PREFIX) || message.author.bot) {
+		return ;
+	}
+
+	const args = message.content.slice(PREFIX.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === "ping") {
 		message.channel.send('Pong!');
-	} else if (message.content === "user-info") {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	} else if (command === "user-info") {
+		message.channel.send(`Your username: ${message.author.username}`);
 	}
 })
 
