98565399882bd0ab824b381de0cf1f3adee4bb45
rajat khanna <31288037+rajatkhanna1999@users.noreply.github.com>
2020-04-22 18:35:58 +0530

    Add files via upload

--- commands/contest.js
+++ commands/contest.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const get_contest = require('../cf/api').get_contest;
-const convertEpochToDate = require('../cf/utilityFunctions').convertEpochToDate;
 
 /**
  * command usage: retrives contests yet to start & User can filter based on division
@@ -9,6 +8,11 @@ const convertEpochToDate = require('../cf/utilityFunctions').convertEpochToDate;
  *   !contest [division]
  *    division can be 1, 2 or 3
  */
+
+ /* Displays the latest upcoming contest of the provided division
+  * If Divison is not specified, it displays the latest contest of each division
+  * and some additional contest
+  */
 
 module.exports = {
     name: 'contest',
@@ -57,17 +61,28 @@ module.exports = {
             return;
         }
 
+        let Div1=0, Div2=0, Div3=0;
         for(const con of valid) {
+            if(con.name.includes(`Div. 1`) && Div1===1)continue;
+            if(con.name.includes(`Div. 2`) && Div2===1)continue;
+            if(con.name.includes(`Div. 3`) && Div3===1)continue;
             const embed = new Discord.MessageEmbed()
                 .setTitle(con.name)
                 .setURL(`http://codeforces.com/contests/${con.id}`)
                 .addField('Type', con.type);
     
-            if(con.startTimeSeconds) embed.addField('Starting', convertEpochToDate(con.startTimeSeconds));
+            if(con.startTimeSeconds)
+            {
+                var TheDate = new Date(con.startTimeSeconds * 1000).toString()
+                embed.addField('Starting', TheDate);
+            }
             if(con.preparedBy) embed.addField('Author', con.preparedBy);
             if(con.difficulty) embed.addField('Difficulty', con.difficulty);
     
             msg.channel.send('', { embed });
+            if(con.name.includes(`Div. 1`))Div1++;
+            if(con.name.includes(`Div. 2`))Div2++;
+            if(con.name.includes(`Div. 3`))Div3++;
         }
 
     },
